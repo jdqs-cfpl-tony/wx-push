ef2d27e4c29c400f385880a9e8ab2685be2ecf0d
jdqs-cfpl-tony <1141495296@qq.com>
2024-09-02 11:02:08 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,10 +1,10 @@
 module.exports = {
-  appid: "wx71941f6106ae1072",
-  appsecret: "b4b2ad8a8d4a94e403831886ac10c646",
-  template_id: "q14lTqsDyK0N-KcZUs8UKHOl3GWn2uErDk-8hbm2ouQ",
+  appid: "wx7860a4ef2be9a6c8",
+  appsecret: "3676bb20d805ec1d45e47daf9dfa2205",
+  template_id: "nRXTHmDDCG8Ljiqs-Px5mQ3Bh5-fDHEcTLKEbPF0dEk",
   touser: "oonWJ5slWuS2OrJdgCLM3GmNdxVk",
 
   key: "201c93c2e2b34bdeecdbf9f5265eb84e",
-  city: "邢台",
-  first_date: "2022-11-10",
+  city: "南京",
+  first_date: "2022-09-28",
 };
